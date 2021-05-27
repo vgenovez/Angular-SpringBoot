c8c88b6e7ffbf73d71297bb2858e97a9c8da6752
Vitor Augusto Genovez <59649115+vgenovez@users.noreply.github.com>
2021-05-26 23:26:12 -0300

    Initial commit

---
+++ src/main/java/io/github/cursodsousa/todo/rest/TodoController.java
@@ -0,0 +1,35 @@
+package io.github.cursodsousa.todo.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import io.github.cursodsousa.todo.model.Todo;
+import io.github.cursodsousa.todo.repository.TodoRepository;
+
+@RestController
+@RequestMapping("/api/todos")
+public class TodoController {
+	
+	@Autowired
+	private TodoRepository repository;
+	
+	@PostMapping
+	public Todo save(@RequestBody Todo todo) {
+		return repository.save(todo);
+	}
+	
+	@GetMapping("{id}")
+	public Todo getById(@PathVariable Long id) {
+		return repository
+				.findById(id)
+				.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));	
+	}
+
+}
